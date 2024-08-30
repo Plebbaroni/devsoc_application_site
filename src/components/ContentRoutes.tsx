31e56af752ef7c4823662fe3987af8909b555717
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2024-08-30 23:34:36 +1000

    Routes again

--- src/components/ContentRoutes.tsx
+++ src/components/ContentRoutes.tsx
@@ -6,6 +6,9 @@ function ContentRoutes() {
     <Routes>
         <Route path='/' element={<Welcome name="Welcome"/>}/>
         <Route path='/AboutMe' element={<AboutMe name="About Me"/>}/>
+        <Route path='/VideoGames' element={<AboutMe name="Video Games"/>}/>
+        <Route path='/Music' element={<AboutMe name="Music"/>}/>
+        <Route path='/OtherStuff' element={<AboutMe name="Other Stuff"/>}/>
     </Routes>
   )
 }
